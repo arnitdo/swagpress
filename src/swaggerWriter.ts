9413f9831cee5c82ac581bebdff4fe1cd6cb3aa1
arnitdo <arnav.nitin.deo@gmail.com>
2022-09-10 20:24:18 +0530

    Update gitignore

--- src/swaggerWriter.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs";
-
-export class SwaggerWriter {
-	outFile: string
-	constructor(outFile: string){
-		this.outFile = outFile
-		console.log("SwaggerWriter initialized successfully")
-	}
-}
